cc52657f7d9b43d849813859e4410338f1f7ea83
Nohan Budry <nohan.budry@icloud.com>
2026-03-02 09:35:26 +0100

    feat: add nvim-jdtls plugin for Java LSP support

---
+++ lua/plugins/jdtls.lua
@@ -0,0 +1,19 @@
+return {
+  "mfussenegger/nvim-jdtls",
+  ft = "java",
+  config = function()
+    local jdtls = require("jdtls")
+    local data_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" ..
+      vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "java",
+      callback = function()
+        jdtls.start_or_attach({
+          cmd = { "jdtls", "-data", data_dir },
+          root_dir = vim.fs.root(0, { "pom.xml", "parent-pom.xml", ".git" }),
+        })
+      end,
+    })
+  end,
+}
